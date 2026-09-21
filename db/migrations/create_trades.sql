a9e9c18c608b4dd43d6ec9374ee992bfbe88c3af
EmoneyCode <trame718@gmail.com>
2026-09-21 15:31:01 -0500

    create_trades table

---
+++ db/migrations/create_trades.sql
@@ -0,0 +1,16 @@
+CREATE TABLE trades (
+    id BIGINT generated always as identity primary key,
+    backtest_id BIGINT NOT NULL REFERENCES backtests(id),
+    timestamp TIMESTAMPTZ not null,
+    side varchar(10) not null,
+    quantity NUMERIC(19,2) not null,
+    execution_price NUMERIC(19,2) not null,
+    commission NUMERIC(19,2) not null,
+    slippage NUMERIC(19,2) not null,
+
+    CONSTRAINT side_check CHECK(side IN('BUY', 'SELL')),
+    CONSTRAINT quantity_check CHECK(quantity > 0),
+    CONSTRAINT execution_check CHECK(execution_price > 0),
+    CONSTRAINT commission CHECK(commission >= 0),
+    CONSTRAINT slippage CHECK(slippage >= 0)
+);
